b18839e739424f16fd1ed3f241d48fc8ccf69e47
Nicolas <nicolas.dg54@outlook.es>
2024-03-13 03:18:02 -0300

    actind4.1 OPCION1 modulo3

--- Modulo3/actividadindividual4.1.nicolasdiazgonzalez/actividadindividual4.1.nicolasdiazgonzalez.py
+++ Modulo3/actividadindividual4.1.nicolasdiazgonzalez/actividadindividual4.1.nicolasdiazgonzalez.py
@@ -1,23 +1,47 @@
+useryclave = {}
 
 def main():
     while True:
         print()
         consulta = int(input('Ingrese el numero de la opción que desea realizar: \n\n1. Crear contraseña. \n\n2. Enviar encuestas. \n\n0. Salir.\n\n'))
         print()
-        if consulta == 1: stock(productos)
-        elif consulta == 2: solicitar(productos, productos_seleccionados)
+        if consulta == 1: user(useryclave)
+        if consulta == 2: enviarencuestas(useryclave)
         elif consulta ==0 : exit()
 
+#OPCION 1
+def user(useryclave):
+    usuario = input('Ingrese su nombre de ususario: ')
+    useryclave[usuario] = ingresarcontrasena()
+    print(f'Contraseña registrada correctamente.\n{''.join((f'Usuario: {usuario}\nClave: {clave}' for usuario, clave in useryclave.items()))}')
+    exit()
 
 def ingresarcontrasena():
-    contrasena = input('Ingrese su contrasena: ')
+    while True:
+        contrasena = input('Ingrese su contrasena: ')
+        if len(validacioncontrasena(contrasena)) > 0:
+            print(f"Su contrasena no cumple con las siguientes condiciones: \n {"\n ".join((f"{condicion}" for condicion in validacioncontrasena(contrasena)))}")
+            continue
+        return contrasena
 
-
-#La contrasena tiene que tener las siguientes condiciones
-condiciones = ['8 o mas caracteres', 'mayusculas', 'minusculas', 'cifras']
-
-def validacioncontrasena(contrasena, condiciones):
+def validacioncontrasena(contrasena):
+    condiciones = ['8 o mas caracteres', 'mayusculas', 'minusculas', 'cifras']
     if len(contrasena) >= 8:
-        print(f'Su contrasena cumple y tiene {condiciones.pop('8 o mas caracteres')}')
-    if not any(letra.isupper() for letra in contrasena):
-        print(f'Su contrasena cumple y tiene {condiciones.pop('mayusculas')}')
+        condiciones.remove('8 o mas caracteres')
+    if any(letra.isupper() for letra in contrasena):
+        condiciones.remove('mayusculas')
+    if any(letra.islower() for letra in contrasena):
+        condiciones.remove('minusculas')
+    if any(letra.isdigit() for letra in contrasena):
+        condiciones.remove('cifras')
+    return condiciones
+
+#FIN OPCION 1
+
+
+#OPCION 2
+
+def enviarencuestas():
+
+
+main()
