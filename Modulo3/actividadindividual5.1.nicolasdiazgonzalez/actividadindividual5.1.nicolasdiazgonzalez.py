009c577e12be65aa9427feb9858adde8bf3a5a8d
Nicolas <nicolas.dg54@outlook.es>
2024-03-15 19:21:08 -0300

    Sprint 3

---
+++ Modulo3/actividadindividual5.1.nicolasdiazgonzalez/actividadindividual5.1.nicolasdiazgonzalez.py
@@ -0,0 +1,7 @@
+usuario1 = {"nombre": "usuario1", "edad": "edad1", "género": "género1", "dirección": "dirección1", "teléfono": "teléfono1"}
+usuario2 = {"nombre": "usuario2", "edad": "edad2", "género": "género2", "dirección": "dirección2", "teléfono": "teléfono2"}
+usuario3 = {"nombre": "usuario3", "edad": "edad3", "género": "género3", "dirección": "dirección3", "teléfono": "teléfono3"}
+usuario4 = {"nombre": "usuario4", "edad": "edad4", "género": "género4", "dirección": "dirección4", "teléfono": "teléfono4"}
+usuario5 = {"nombre": "usuario5", "edad": "edad5", "género": "género5", "dirección": "dirección5", "teléfono": "teléfono5"}
+usuario6 = {"nombre": "usuario6", "edad": "edad6", "género": "género6", "dirección": "dirección6", "teléfono": "teléfono6"}
+usuario7 = {"nombre": "usuario7", "edad": "edad7", "género": "género7", "dirección": "dirección7", "teléfono": "teléfono7"}
